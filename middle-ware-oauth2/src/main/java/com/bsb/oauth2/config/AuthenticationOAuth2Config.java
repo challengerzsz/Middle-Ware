8fe7fa56bd77d3c13b66d99c3f87ee9e8ee485ec
challengerzsz <challengerzsz@126.com>
2018-10-19 17:01:32 +0800

    修改模块间依赖的bug maven模块拆分完成

---
+++ middle-ware-oauth2/src/main/java/com/bsb/oauth2/config/AuthenticationOAuth2Config.java
@@ -0,0 +1,13 @@
+package com.bsb.oauth2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * @author zeng on 18-10-19.
+ * @version 1.0
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthenticationOAuth2Config {
+}
--- middle-ware-server/src/main/java/com/bsb/server/ServerApplication.java
+++ middle-ware-server/src/main/java/com/bsb/server/ServerApplication.java
@@ -7,10 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author zeng on 18-10-19.
  * @version 1.0
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bsb.oauth2", "com.bsb.websocket"})
 public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
+
 }
