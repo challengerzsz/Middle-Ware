a0fd1db839241bc260b1bb4574821e96e42affb2
challengerzsz <challengerzsz@126.com>
2018-10-19 09:39:29 +0800

    模块化拆分基于oauth2协议的消息推送中间件，后期服务化治理

---
+++ middle-ware-server/src/main/java/com/bsb/server/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.bsb.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zeng on 18-10-19.
+ * @version 1.0
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
---
+++ middle-ware-websocket/src/main/java/com/bsb/websocket/Main.java
@@ -0,0 +1,12 @@
+package com.bsb.websocket;
+
+/**
+ * @author zeng on 18-10-19.
+ * @version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("success");
+    }
+}
